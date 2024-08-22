01d91eb4171dd3f8332c6f7315d60255c5b27519
AashisMhj <aashismhj5511@gmail.com>
2024-08-22 12:32:46 +0545

    added next cruip landing page

---
+++ cruip-landing-page/next-cruip-landing-page/README.md
@@ -0,0 +1,1 @@
+# next Cruip Landing Page
---
+++ cruip-landing-page/next-cruip-landing-page/src/app/(default)/layout.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/ui/header";
+import Footer from "@/components/ui/footer";
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+    <>
+      <Header />
+
+      <main className="grow">{children}</main>
+
+      <Footer border={true} />
+    </>
+  );
+}
